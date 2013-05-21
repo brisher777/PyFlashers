40753e1e27300972411e9833d6c49c2070cf0a03
brisher777 <brisher777@gmail.com>
2013-05-20 21:16:59 -0500

    added framework for file open and save as
    also initialized three text boxes
    -- need ideas on a decent looking interface... --

--- src/PyFlashers.py
+++ src/PyFlashers.py
@@ -2,6 +2,30 @@
 Created on May 18, 2013
 
 @author: ben
+
+-- TODO --
+  if a number isn't in a regex of the tree, keep processing new input:
+      else show that number's question/answer
+  decide on user interaction; enter, button, etc...
+  when opening a file, only look for xml
+
+-- END -- 
+
+-- LAYOUT -- 
+3 main entry fields for I/O
+    1 number
+    1 question
+    1 answer
+    
+2 full windows, both derived from a root window or the root window will be input with an option to start regurgitation in a child window
+    1 for input
+          twitter length messages for input?
+    1 for regurgitation
+        for regurg, fill in the blank for the answer and then compare to the original
+-- END -- 
+
+options to save or open the file 
+
 '''
 
 import Tkinter
@@ -18,14 +42,20 @@ class FlashCard(Tkinter.Tk):
     def initialize(self):
         self.grid()
         
-
-        self.entry_question = Tkinter.Entry()
         self.question_var = Tkinter.StringVar()
+        self.entry_question = Tkinter.Entry(self, textvariable = self.question_var)
         self.question_var.set('this is a question maybe?')
         self.entry_question.grid(row = 0, column = 1)
         
+        self.answer_var = Tkinter.StringVar()
+        self.entry_answer = Tkinter.Entry(self, textvariable = self.answer_var)
+        self.answer_var.set('this is a question maybe?')
+        self.entry_answer.grid(row = 1, column = 1)
         
-        
+        self.number_var = Tkinter.StringVar()        
+        self.entry_number = Tkinter.Entry(self, textvariable = self.number_var)
+        self.number_var.set('this is a question maybe?')
+        self.entry_number.grid(row = 2, column = 1)
         
         
         
@@ -36,10 +66,26 @@ class FlashCard(Tkinter.Tk):
         self['menu'] = self.menu_bar 
         self.file_menu = Tkinter.Menu(self.menu_bar)  
         self.menu_bar.add_cascade(label = 'File', menu=self.file_menu)
-        self.file_menu.add_command(label = 'Open')#, command = self.open_file)
-        self.file_menu.add_command(label = 'Save as...')#, command = self.save_as)
+        self.file_menu.add_command(label = 'Open', command = self.open_file)
+        self.file_menu.add_command(label = 'Save as...', command = self.save_as)
         
-        
+    def save_as(self):
+        file_name = tkFileDialog.asksaveasfilename(parent = self, title = 'Save the file as...')
+        if len(file_name) > 0:
+            saved_file = open('%s' % file_name, 'w')
+            print 'you saved a file'
+            #file.write('you saved a file') ## actually write something relevant later
+            saved_file.close()
+            
+    def open_file(self):
+        file_name = tkFileDialog.askopenfilename(parent = self, title = 'Open file...')
+        opened_file = open('%s' % file_name, 'r')
+        line = opened_file.readline()
+        ## check for a non-null file
+        ## open it in the appropriate way
+        print 'you opened a file'
+            
+            
     ## child window generator
     ## generic for now
     ## one window will be for generation
--- src/PyFlashers_defines.py
+++ src/PyFlashers_defines.py
@@ -2,10 +2,6 @@
 Created on May 18, 2013
 
 @author: ben
--- ask the user for input, possibly a GUI, more easily a file --
--- print out a template at the beginning of each run and direct the user to it? -- 
--- build a dictionary, question as the key, answer as the item -- 
--- for user input, you could read the last char of their input and replace it with a question mark if it's not a question mark --
 '''
 
 import xml.etree.ElementTree as ET
